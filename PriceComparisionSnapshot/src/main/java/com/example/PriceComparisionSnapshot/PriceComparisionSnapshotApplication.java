2fd7f7ff5929de18d7610bd4b83cc00ea232cafa
kathirvel962 <kathirvel.s2024cse@sece.ac.in>
2025-10-23 22:12:55 +0530

    Updated project with all changes

---
+++ PriceComparisionSnapshot/src/main/java/com/example/PriceComparisionSnapshot/PriceComparisionSnapshotApplication.java
@@ -0,0 +1,13 @@
+package com.example.PriceComparisionSnapshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparisionSnapshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceComparisionSnapshotApplication.class, args);
+	}
+
+}
---
+++ PriceComparisionSnapshot/src/test/java/com/example/PriceComparisionSnapshot/PriceComparisionSnapshotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PriceComparisionSnapshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparisionSnapshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
